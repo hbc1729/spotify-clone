a8ec1970e9ff295ace22da8a3984a04e7e7f7a7c
Chiranth <chiranth1729@gmail.com>
2023-10-26 06:38:57 +0530

    All changes

---
+++ components/Player.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import useGetSongById from "@/hooks/useGetSongById";
+import useLoadSongUrl from "@/hooks/useLoadSongUrl";
+import usePlayer from "@/hooks/usePlayer";
+import PlayerContent from "./PlayerContent";
+
+const Player = () => {
+  const player = usePlayer();
+  const { song } = useGetSongById(player.activeId);
+
+  const song_url = useLoadSongUrl(song!);
+
+  if (!song || !song_url || !player.activeId) {
+    return null;
+  }
+
+  return (
+    <div className="fixed bottom-0 bg-black w-full py-2 h-[80px] px-4">
+      <PlayerContent key={song_url} song={song} songUrl={song_url} />
+    </div>
+  );
+};
+
+export default Player;
---
+++ providers/ModalProvider.tsx
@@ -0,0 +1,26 @@
+"use client";
+import AuthModal from "@/components/AuthModal";
+import SubscribeModal from "@/components/SubscribeModal";
+import UploadModal from "@/components/UploadModal";
+import { ProductWithPrice } from "@/types";
+import { useState, useEffect } from "react";
+
+const ModalProvider = ({ products }: { products: ProductWithPrice[] }) => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) return null;
+
+  return (
+    <>
+      <AuthModal />
+      <UploadModal />
+      <SubscribeModal products={products} />
+    </>
+  );
+};
+
+export default ModalProvider;

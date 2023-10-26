2b7aa7fd286426b7d95f3d0d82ebad1b5beea374
Chiranth <chiranth1729@gmail.com>
2023-10-26 07:59:55 +0530

    Removed github authentication

--- components/AuthModal.tsx
+++ components/AuthModal.tsx
@@ -41,7 +41,7 @@ const AuthModal = () => {
       <Auth
         theme="dark"
         magicLink
-        providers={["github"]}
+        providers={[]}
         supabaseClient={supabseClient}
         appearance={{
           theme: ThemeSupa,
